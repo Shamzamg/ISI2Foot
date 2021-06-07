df177eff4a50efda9daac55f60eecbc7d3e48bba
Shamzamg <shamsdine.ziani@etu.univ-lyon1.fr>
2021-06-07 12:27:59 +0200

    new players for database

--- database/seeders/JoueurSeeder.php
+++ database/seeders/JoueurSeeder.php
@@ -84,6 +84,96 @@ class JoueurSeeder extends Seeder
                 'numero' => 20,
                 'photo' => 'slimani.png'
             ],
+
+            [
+                'id' => 8,
+                'equipe_id' => 1,
+                'nom' => 'Mendy',
+                'prenom' => 'Ferland',
+                'age' => 25,
+                'numero' => 23,
+                'photo' => 'mendy.png'
+            ],
+
+            [
+                'id' => 9,
+                'equipe_id' => 1,
+                'nom' => 'Militão',
+                'prenom' => 'Éder',
+                'age' => 23,
+                'numero' => 3,
+                'photo' => 'militao.png'
+            ],
+
+            [
+                'id' => 10,
+                'equipe_id' => 1,
+                'nom' => 'Carvajal',
+                'prenom' => 'Dani',
+                'age' => 28,
+                'numero' => 2,
+                'photo' => 'carvajal.png'
+            ],
+
+            [
+                'id' => 11,
+                'equipe_id' => 1,
+                'nom' => 'Varane',
+                'prenom' => 'Raphaël',
+                'age' => 27,
+                'numero' => 5,
+                'photo' => 'varane.png'
+            ],
+
+            [
+                'id' => 12,
+                'equipe_id' => 1,
+                'nom' => 'Nacho',
+                'prenom' => '',
+                'age' => 30,
+                'numero' => 6,
+                'photo' => 'nacho.png'
+            ],
+
+            [
+                'id' => 13,
+                'equipe_id' => 1,
+                'nom' => 'Ramos',
+                'prenom' => 'Sergio',
+                'age' => 35,
+                'numero' => 4,
+                'photo' => 'ramos.png'
+            ],
+
+            [
+                'id' => 14,
+                'equipe_id' => 1,
+                'nom' => 'Hazard',
+                'prenom' => 'Eden',
+                'age' => 29,
+                'numero' => 7,
+                'photo' => 'hazard.png'
+            ],
+
+            [
+                'id' => 15,
+                'equipe_id' => 1,
+                'nom' => 'Casemiro',
+                'prenom' => '',
+                'age' => 28,
+                'numero' => 14,
+                'photo' => 'casemiro.png'
+            ],
+
+            [
+                'id' => 16,
+                'equipe_id' => 1,
+                'nom' => 'Courtois',
+                'prenom' => 'Thibault',
+                'age' => 28,
+                'numero' => 1,
+                'photo' => 'courtois.png'
+            ],
     
         ]);
     }
