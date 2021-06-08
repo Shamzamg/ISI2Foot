e15f70df549afcdf0861eeb08ee7fd2caec076ce
Shamzamg <shamsdine.ziani@etu.univ-lyon1.fr>
2021-06-08 19:06:07 +0200

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,13 +19,13 @@
                     @auth
                     <div class="row">
                         <div class="col-lg-4">
-                            <a href="{{ route('joueurs.show', $joueur->id) }}" class="btn-small btn-primary">Voir</a>
+                            <a href="{{ route('joueurs.show', $joueur->id) }}" class="btn-small">Voir</a>
                         </div>
                         <div class="col-lg-4">
-                            <a href="{{ route('joueurs.edit', $joueur->id) }}" class="btn-small btn-info">Modifier</a>
+                            <a href="{{ route('joueurs.edit', $joueur->id) }}" class="btn-small">Modifier</a>
                         </div>
                         <div class="col-lg-4">
-                            <a href="{{ route('joueurs.destroy', $joueur->id) }}" method="post" class="btn-small btn-warning" type="submit">Supprimer</button>
+                            <a href="{{ route('joueurs.destroy', $joueur->id) }}" method="post" class="btn-small" type="submit">Supprimer</button>
                         </div>
                     </div>
                     @else
