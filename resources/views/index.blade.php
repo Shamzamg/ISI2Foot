27db78f59cd7e5459fa2cdcb75c568ae0ebbc7b1
Shamzamg <shamsdine.ziani@etu.univ-lyon1.fr>
2021-06-08 19:00:13 +0200

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,13 +17,21 @@
                 <img src="{{ asset('images/joueurs/'.$joueur->photo) }}" />
                 <div class="desc">
                     @auth
-                    <a href="{{ route('joueurs.show', $joueur->id) }}" class="btn btn-primary btn-sm">Voir</a>
-                    <a href="{{ route('joueurs.edit', $joueur->id) }}" class="btn btn-info btn-sm">Modifier</a>
-                    <form action="{{ route('joueurs.destroy', $joueur->id) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-warning btn-sm" type="submit">Supprimer</button>
-                    </form>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <a href="{{ route('joueurs.show', $joueur->id) }}" class="btn btn-primary btn-sm">Voir</a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="{{ route('joueurs.edit', $joueur->id) }}" class="btn btn-info btn-sm">Modifier</a>
+                        </div>
+                        <div class="col-lg-4">
+                            <form action="{{ route('joueurs.destroy', $joueur->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-warning btn-sm" type="submit">Supprimer</button>
+                            </form>
+                        </div>
+                    </div>
                     @else
                     <a href="{{ route('joueurs.show', $joueur->id) }}" class="btn">Voir</a>
                     @endauth
